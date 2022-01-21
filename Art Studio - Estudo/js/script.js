f576888524d3428776e9a4e75edb0cfc76d83614
Felipe Santos Lino <43026998842@info.scs>
2022-01-21 11:10:37 -0300

    Header terminado com responsivo

---
+++ Art Studio - Estudo/js/script.js
@@ -0,0 +1,32 @@
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () => {
+    navbar.classList.toggle('active');
+    searchForm.classList.remove('active');
+    cartItem.classList.remove('active');
+    console.log('teste');
+  };
+  
+  let cartItem = document.querySelector('.cart-items-container');
+  
+  document.querySelector('#cart-btn').onclick = () => {
+    cartItem.classList.toggle('active');
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+    console.log('teste');
+  };
+
+  let searchForm = document.querySelector('.search-form');
+  
+  document.querySelector('#search-btn').onclick = () => {
+    searchForm.classList.toggle('active');
+    navbar.classList.remove('active');
+    cartItem.classList.remove('active');
+    console.log('teste');
+  };
+
+  window.onscroll = () =>{
+      navbar.classList.remove('active');
+      searchForm.classList.remove('active');
+      cartItem.classList.remove('active');
+  }
